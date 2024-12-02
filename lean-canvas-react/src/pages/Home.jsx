269b28eb7244c2b5b4bbd7317ddebe23337ec42c
LEE WOOJIN <24864@naver.com>
2024-12-02 12:30:10 +0900

    검색 카테고리필터

--- lean-canvas-react/src/pages/Home.jsx
+++ lean-canvas-react/src/pages/Home.jsx
@@ -7,9 +7,19 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import Loading from '../components/Loading';
 import Error from '../components/Error';
 import Button from '../components/Button';
+import CategoryFilter from '../components/CategoryFilter';
 
 function Home() {
-  const [searchText, setSearchText] = useState();
+  // const [searchText, setSearchText] = useState();
+  const [filter, setFilter] = useState({
+    searchText: undefined,
+    category: undefined,
+  });
+  const handleFilter = (key, value) =>
+    setFilter({
+      ...filter,
+      [key]: value,
+    });
   const [isGridView, setIsGridView] = useState(true);
   // const [data, setData] = useState([]);
 
@@ -17,8 +27,9 @@ function Home() {
 
   // 1] 데이터 조회
   const { data, isLoading, error, refetch } = useQuery({
-    queryKey: ['canvases', searchText],
-    queryFn: () => getCanvases({ title_like: searchText }),
+    queryKey: ['canvases', filter.searchText, filter.category],
+    queryFn: () =>
+      getCanvases({ title_like: filter.searchText, category: filter.category }),
     initialData: [],
   });
 
@@ -49,7 +60,16 @@ function Home() {
   return (
     <>
       <div className="mb-6 flex flex-col sm:flex-row items-center justify-between">
-        <SearchBar searchText={searchText} setSearchText={setSearchText} />
+        <div className="flex gap-2 flex-col w-full sm:flex-row mb-4 sm:mb-0">
+          <SearchBar
+            searchText={filter.searchText}
+            onSearch={val => handleFilter('searchText', val)}
+          />
+          <CategoryFilter
+            category={filter.category}
+            onChange={val => handleFilter('category', val)}
+          />
+        </div>
         <ViewToggle setIsGridView={setIsGridView} isGridView={isGridView} />
       </div>
       <div className="flex justify-end mb-6">
@@ -63,7 +83,7 @@ function Home() {
         <CanvasList
           filteredData={data}
           isGridView={isGridView}
-          searchText={searchText}
+          searchText={filter.searchText}
           onDeleteItem={handleDeleteItem}
         />
       )}
